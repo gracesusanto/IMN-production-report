86d9e60ed9658b09d5493c7b33d15656f7f5b839
Grace Susanto <susanto.grace98@gmail.com>
2023-06-21 23:40:53 -0700

    fix format

--- services/web/app/cmd/generate_report.py
+++ services/web/app/cmd/generate_report.py
@@ -195,7 +195,7 @@ def _generate_keterangan_limax(row):
 
     if keterangan[-2::] == ", ":
         keterangan = keterangan[:-2]
-    return f'"{keterangan}"'
+    return f"\"{keterangan}\""
 
 
 def _format_time_for_limax(time):
@@ -338,7 +338,7 @@ def get_report(
     df["Reject"] = df["Reject"].astype(int)
     df["Rework"] = df["Rework"].astype(int)
 
-    df["Plant"] = df["MC"].apply(lambda MC: MC[-1])
+    df["Plant"] = df["MC"].apply(lambda MC: f"'MC[-1]")
     df["Awal"] = df["StartTime"].apply(_format_time_for_limax)
     df["Akhir"] = df["StopTime"].apply(_format_time_for_limax)
     df["Kode Keterangan"] = df["Desc"].apply(lambda Desc: Desc[0:2].strip())
