8ab4503c55dc6779611c4cd4effc20d887c28f1f
Grace Susanto <susanto.grace98@gmail.com>
2024-01-21 13:51:35 -0800

    add imn ip

--- services/web/app/main.py
+++ services/web/app/main.py
@@ -29,6 +29,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:3000",
         "http://192.168.0.103:3000",
+        "http://192.168.0.218:3000/",
     ],  # Allows all origins
     allow_credentials=True,
     allow_methods=["*"],  # Allows all methods
